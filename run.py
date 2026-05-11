3cb0a0d2303b1b2f50b9b2aefe6603a9d46a8f14
Abbos-Amirov <abbosamirov571@gmail.com>
2026-05-11 20:59:56 +0900

    feat: python system varibales & cpecial methods via dunders

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
-a = 100
-print("a", a)
-message = "salaom dunyo"
+# __Dunder__ builtins __init__
+message = "PYTHON: Everything is object!"
 print(message)
-message = "salaom dunyo"
-print(message)
+
+result = type(message)
+print("result:", result)
+
+''' In Python, there are builtin tools:
+(1) TYPES > int float str list dict
+(2) FUNCTIONS > print() len() input() type() str() int()
+(3) CONSTANTS > True False None
+'''
+
+print(dir(__builtins__))
